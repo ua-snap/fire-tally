fc373ab1010a49b3c2e99e85e6c7b1f29e952db3
Bruce Crevensten <bruce.crevensten@gmail.com>
2020-04-15 14:51:40 -0700

    Changes to chart
     * Moves some data pre-processing out of the update function
     * Slices date range to April 1 ... today or at least 45 days worth of data
     * Removes the x-range slider
     * Updates test data set
     * Adds 2020 to legend

--- luts.py
+++ luts.py
@@ -10,7 +10,7 @@ description = "This visualization compares the current year to all high fire yea
 
 default_style = {"color": "rgba(0, 0, 0, 0.25)", "width": 1}
 
-important_years = [2004, 2005, 2009, 2013, 2015, 2019]
+important_years = [2004, 2005, 2009, 2013, 2015, 2019, 2020]
 years_lines_styles = {
     "2004": {"color": "rgba(100, 143, 255, 1)", "width": "2"},
     "2005": {"color": "rgba(120, 94, 240, 1)", "width": "2"},
