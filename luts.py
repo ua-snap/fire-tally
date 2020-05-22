e8b22594c097bf7ed7aa1ade889646bcb4e91917
BobTorgerson <rltorgerson@alaska.edu>
2020-05-21 18:06:18 -0800

    Revert "Adds in "original mode" graph."
    This reverts commit 653384e0880d42dde54a06227ca1df65c4e4c843.

--- luts.py
+++ luts.py
@@ -8,11 +8,9 @@ url = "http://snap.uaf.edu/tools/demo"
 preview = "http://snap.uaf.edu/tools/demo/assets/preview.png"
 description = "This visualization compares the current year to all high fire years (> 1 million acres burned) since daily tally records began in 2004."
 
-default_date_range = [91, 260]
-
 default_style = {"color": "rgba(0, 0, 0, 0.25)", "width": 1}
 
-important_years = [2004, 2005, 2009, 2010, 2013, 2015, 2019, 2020]
+important_years = [2004, 2005, 2009, 2013, 2015, 2019, 2020]
 years_lines_styles = {
     "2004": {"color": "rgba(100, 143, 255, 1)", "width": "2"},
     "2005": {"color": "rgba(120, 94, 240, 1)", "width": "2"},
@@ -20,7 +18,7 @@ years_lines_styles = {
     "2007": default_style,
     "2008": default_style,
     "2009": {"color": "rgba(220, 38, 127, 1)", "width": "2"},
-    "2010": {"color": "rgba(10, 128, 64, 1)", "width": "2"},
+    "2010": default_style,
     "2011": default_style,
     "2012": default_style,
     "2013": {"color": "rgba(254, 97, 0, 1)", "width": "2"},
@@ -29,7 +27,7 @@ years_lines_styles = {
     "2016": default_style,
     "2017": default_style,
     "2018": default_style,
-    "2019": {"color": "rgba(10, 255, 128, 1)", "width": "2"},
+    "2019": {"color": "rgba(10, 255, 128, .85)", "width": "2"},
     "2020": {"color": "rgba(10, 25, 0, .85)", "width": "4"},
 }
 
